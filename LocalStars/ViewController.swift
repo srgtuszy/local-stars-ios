4d92a7453c06cf99cb4d3e5f542a0eb324577f9b
Michal Tuszynski <srgtuszy@gmail.com>
2020-11-20 23:24:58 +0100

    Initial Commit

---
+++ LocalStars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalStars
+//
+//  Created by Michal Tuszynski on 20/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
