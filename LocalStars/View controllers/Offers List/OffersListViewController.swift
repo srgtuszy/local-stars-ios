7d13ee952c661c57b99bd8bb8f31be33c515d4fb
Peter Tuszynski <thepetertee@gmail.com>
2020-11-21 01:14:27 +0100

    offers UI structure

---
+++ LocalStars/View controllers/Offers List/OffersListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OffersListViewController.swift
+//  LocalStars
+//
+//  Created by Piotr Tuszynski on 21/11/2020.
+//
+
+import Foundation
+import UIKit
+
+
+class OffersListViewController: UIViewController {
+    
+}
