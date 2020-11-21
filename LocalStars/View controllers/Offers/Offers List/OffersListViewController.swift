0b1b93ea55e403918dad042e81fc3d799600b4f3
Michal Tuszynski <srgtuszy@gmail.com>
2020-11-21 21:41:14 +0100

    Offers list navigation works

--- LocalStars/View controllers/Offers/Offers List/OffersListViewController.swift
+++ LocalStars/View controllers/Offers/Offers List/OffersListViewController.swift
@@ -11,7 +11,7 @@ import Kingfisher
 
 class OffersListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
-    private let offerFetcher = EntityFetcher()
+    private let fetcher = EntityFetcher()
     private let dataSource = OffersDataSource()
 
     override func viewDidLoad() {
@@ -20,17 +20,39 @@ class OffersListViewController: UIViewController {
         tableView.separatorStyle = .none
         tableView.separatorInset = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0)
         tableView.rowHeight = 200
+        tableView.delegate = self
         dataSource.attach(tableView: tableView)
         configureOfferUpdates()
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let viewController = segue.destination as! OfferDetailsViewController
+        let (merchant, offer) = sender as! (Merchant, Offer)
+        viewController.merchant = merchant
+        viewController.offer = offer
+    }
+
     private func configureOfferUpdates() {
-        offerFetcher.startUpdatingOffers {[weak self] result in
+        fetcher.startUpdatingOffers {[weak self] result in
             switch result {
             case .success(let offers):
                 self?.dataSource.update(with: offers)
             case .failure(let error):
                 print("Failed to fetch offers: \(error)")
+            }
+        }
+    }
+}
+
+extension OffersListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let offer = dataSource.offers[indexPath.row]
+        fetcher.fetch(merchantWith: offer.merchantId) {[weak self] result in
+            switch result {
+            case .success(let merchant):
+                self?.performSegue(withIdentifier: "showOffer", sender: (merchant, offer))
+            case .failure(let error):
+                print("Failed to fetch merchant: \(error)")
             }
         }
     }
