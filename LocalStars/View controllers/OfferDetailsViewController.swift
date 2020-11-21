269251a39b9c80fc3ba94209271957b8a020cbb6
Michal Tuszynski <srgtuszy@gmail.com>
2020-11-21 20:17:22 +0100

    Offer details WIP

--- LocalStars/View controllers/OfferDetailsViewController.swift
+++ LocalStars/View controllers/OfferDetailsViewController.swift
@@ -6,12 +6,35 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class OfferDetailsViewController: UIViewController {
+    @IBOutlet private var offerImageView: UIImageView!
+    @IBOutlet private var offerTitleLabel: UILabel!
+    @IBOutlet private var offerDescriptionLabel: UILabel!
+    @IBOutlet private var addressLabel: UILabel!
     var merchant: Merchant!
     var offer: Offer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadImage()
+    }
+
+    private func loadImage() {
+        if let url = URL(string: offer.photoUrl) {
+            let processor = DownsamplingImageProcessor(size: offerImageView.bounds.size)
+                         |> RoundCornerImageProcessor(cornerRadius: 20)
+            offerImageView.kf.indicatorType = .activity
+            offerImageView.kf.setImage(with: url, options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(1)),
+            ])
+        }
+    }
+
+    @IBAction @objc private func didTapBuyButton() {
+
     }
 }
