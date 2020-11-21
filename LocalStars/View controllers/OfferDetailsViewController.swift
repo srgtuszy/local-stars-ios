c1e34d4101a3b77506504215804627fc75b153e9
Michal Tuszynski <srgtuszy@gmail.com>
2020-11-21 17:57:54 +0100

    Navigation skeleton for offer details

---
+++ LocalStars/View controllers/OfferDetailsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OfferDetailsViewController.swift
+//  LocalStars
+//
+//  Created by Michal Tuszynski on 21/11/2020.
+//
+
+import UIKit
+
+final class OfferDetailsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
