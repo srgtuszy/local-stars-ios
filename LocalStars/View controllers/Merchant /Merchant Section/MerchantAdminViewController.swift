50f104418c058c55a469ef60e611b86b8c530d3d
Michal Tuszynski <srgtuszy@gmail.com>
2020-11-22 01:26:42 +0100

    Added merchant offers to admin

--- LocalStars/View controllers/Merchant /Merchant Section/MerchantAdminViewController.swift
+++ LocalStars/View controllers/Merchant /Merchant Section/MerchantAdminViewController.swift
@@ -9,5 +9,28 @@ import Foundation
 import UIKit
 
 class MerchantAdminViewController: UIViewController {
-    
+    @IBOutlet weak var tableView: UITableView!
+    private let dataSource = OffersDataSource()
+    private let offerFetcher = EntityFetcher()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource.attach(tableView: tableView)
+        fetchOffers()
+    }
+
+    private func fetchOffers() {
+        offerFetcher.fetch(offersFromMearchantWith: .merchantId) {[weak self] result in
+            switch result {
+            case .success(let offers):
+                self?.dataSource.update(with: offers)
+            case .failure(let error):
+                print("failed to fetch offers: \(error)")
+            }
+        }
+    }
+}
+
+private extension String {
+    static let merchantId = "rBabB8LvKR0VPJcQBA0o"
 }
